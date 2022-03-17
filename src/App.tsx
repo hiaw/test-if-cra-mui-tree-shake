bffa06417da4062f2109862187b869cffc97628d
Daniel Chong <danielgq.chong@gmail.com>
2022-03-17 16:27:39 +1300

    yeap. DataGrid is a big package

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Box } from "@mui/material";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+
+const rows: GridRowsProp = [
+  { id: 1, col1: "Hello", col2: "World" },
+  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+  { id: 3, col1: "MUI", col2: "is Amazing" },
+];
+
+const columns: GridColDef[] = [
+  { field: "col1", headerName: "Column 1", width: 150 },
+  { field: "col2", headerName: "Column 2", width: 150 },
+];
 
 function App() {
   return (
@@ -12,6 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Box />
+        <DataGrid rows={rows} columns={columns} />
         <a
           className="App-link"
           href="https://reactjs.org"
